bd099eb9a6ec29879e0e5e4e4405660cda8e94e0
jewelsosa <157390894+jewelsosa@users.noreply.github.com>
2024-01-29 16:02:02 +0000

    //commiting celsius convert to fahrenheit project

---
+++ mhep.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //declare variable 
+    double celsiusNum;
+    double fahrenNum;
+    //get celsius # from user 
+    cout << "What celsius temp would you like to convert to?"
+    cin >> celsiusNum
+    //convert to fahrenheit
+    fahrenheit = (celsiusNum * (9/5.)) + 32;
+    //output to console new value
+    cout << celsiusNum << " is " << fahrenNum << " in fahrenheit. " << endl;
+    return 0;
+}
