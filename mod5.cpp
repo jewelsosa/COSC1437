67510415dfa8ee71035c7613478637d8592c9071
jewelsosa <157390894+jewelsosa@users.noreply.github.com>
2024-02-14 16:11:46 +0000

    dfgbrfg

--- mod5.cpp
+++ mod5.cpp
@@ -12,9 +12,24 @@ cin >> lastname;
 char firstIn = firstname[0];
 char lastin = lastname[0];
 cout << "your initials are " << firstIn << ". " << lastin << ".";
-string newfirst = firstname.length();
-string newlast = lastname.length();
-cout << "your new name is now " << 
+// this is the part where i try my best
+string fullname = firstname + " " + lastname;
+getline(cin, fullname);
+//i tried using a standard array, so now im using ANOtHER array to get the chunks of strings
+// i did ask for help and watch youtube for this
+size_t spacePos = fullname.find(' ');
+
+firstname = fullname.substr(0, spacePos);
+lastname = fullname.substr(spacePos + 1);
+
+size_t firsthalf = firstname.length() / 2;
+size_t lasthalf = lastname.length() / 2;
+
+string newfirst = firstname.substr(0, firsthalf) + lastname.substr(lasthalf);
+string  newlast = lastname.substr(0, lasthalf) + firstname.substr(firsthalf);
+
+string newfull = newfirst + newlast;
+cout << "your new name is now " << newfull;
 
 return 0;
 }
